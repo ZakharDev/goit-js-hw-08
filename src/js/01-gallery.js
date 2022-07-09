a94bbd5818b39dc051198b57128a64a0127733b1
ZakharDev <zdevatko9@gmail.com>
2022-07-09 19:14:58 +0300

    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryLightBox = document.querySelector('.gallery');
+const itemGallery = createGalletyItem(galleryItems);
+
+galleryLightBox.insertAdjacentHTML('beforeend', itemGallery);
+galleryLightBox.addEventListener('click', onPictureClick);
+
+function createGalletyItem(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            </a>
+        `;
+    }).join('');
+};
+
+function onPictureClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+    let lightbox = new SimpleLightbox('.gallery a', { 
+        preloading: false,
+        alertError: false
+     });
+    console.log(lightbox);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
+
+let formData = {};
+const FEEDBACK = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+onSaveMessage();
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  const storage = JSON.stringify(formData);
+  localStorage.setItem(FEEDBACK, storage);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK);
+
+  console.log(formData);
+
+  for (const key in formData) {
+    delete formData[key];
+  }
+}
+
+function onSaveMessage() {
+  const saveMessage = localStorage.getItem(FEEDBACK);
+  const save = JSON.parse(saveMessage);
+
+  if (save === null) {
+  input.value = '';
+  textarea.value = '';
+  } else if (save.email === undefined && save.message !== undefined) {
+    input.value = '';
+    textarea.value = save.message;
+  } else if (save.email !== undefined && save.message === undefined) {
+    input.value = save.email;
+    textarea.value = '';
+  } else {
+    input.value = save.email;
+    textarea.value = save.message;
+  }
+}
 
